2d65eca9c9e5997e317c1f44db8bfef06596e75b
liangxiaobao <liang_jv@163.com>
2018-06-29 11:10:21 +0800

    629_3

---
+++ first-web/src/com/xiao/event/eventController1.java
@@ -0,0 +1,37 @@
+package com.xiao.event;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class eventController1 {
+
+    /**
+     *
+     */
+    @RequestMapping("/001")
+    public String index(Model model){
+        model.addAttribute("11",111);
+        return "001";
+    }
+
+    @RequestMapping("/002")
+    public String index2(Model model){
+        model.addAttribute("22",222);
+        return "002";
+    }
+
+    @RequestMapping("/003")
+    public String index3(Model model){
+        model.addAttribute("33",333);
+        return "003";
+    }
+
+    @RequestMapping("/004")
+    public String index4(Model model){
+        model.addAttribute("44",444);
+        return "004";
+    }
+
+}
