cc961c592a6035e9ff1efb3595a0c7b0fedd890a
liangxiaobao <liang_jv@163.com>
2018-06-29 11:30:00 +0800

    629_5

--- first-web/src/com/xiao/event/eventController2.java
+++ first-web/src/com/xiao/event/eventController2.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class eventController1 {
+public class eventController2 {
 
     /**
      *
